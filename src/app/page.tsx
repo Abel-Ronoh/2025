e022d3cefb8da515a1d38d121690d99cb6b57a0e
Abel-Ronoh <abellronoh@gmail.com>
2025-05-08 18:45:21 +0300

    initial portfolio update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,45 @@
-import Image from "next/image";
-
+import Image from 'next/image';
+import img from '../../public/emg.png'
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="bg-[#073737] flex flex-col items-center">
+      <div className="absolute h-[100vh] w-[300px] right-0 top-0 bg-[#FFF2E2]"></div>
+      <div className="bg-[#FDFDFD] w-11/12 mt-10 p-1 rounded-4xl z-10">
+      <div className='hidden lg:flex md:bg-red-300 md:w-full'>
+        <h1>HOME</h1>
+        <h1>SKILL</h1>
+        <h1>EXPERIENCE</h1>
+        <button className="bg-transparent border-4 border-[#0A3638] w-[96px] h-[43px] text-[#0A3638]  m-1 font-[14px] hover:border-[#577955] hover:bg-[#577955] hover:text-white cursor-pointer ">
+          GitHub
+        </button>
+      </div>
+      <div className="mt-12 md:mt-16 px-5">
+        <h1 className="text-[14px] md:text-5xl font-bold mb-[7px] ">HELLO</h1>
+        <h2 className="text-[32px] md:text-3xl font-light  mb-[7px]">I'm <span className="text-[#E1B890] font-semibold">Abel Ronoh.</span></h2>
+        <h3 className="text-[32px] md:text-2xl font-bold mb-[7px] text-[#073737]">Software Engineer</h3>
+        
+        <p className="text-[14px] md:text-xl text-gray-600 leading-relaxed mb-8">
+          A Software Engineer based on the web.
+          Building full-stack web applications with a
+          focus on the overall architecture and the front
+        </p>
+        
+        <button className="bg-[#0A3638] w-[96px] h-[43px] text-white m-1  font-[14px] hover:bg-[#577955] cursor-pointer ">
+          Let's Talk
+        </button>
+        <button className="bg-transparent border-4 border-[#0A3638] w-[96px] h-[43px] text-[#0A3638]  m-1 font-[14px] hover:border-[#577955] hover:bg-[#577955] hover:text-white cursor-pointer ">
+          GitHub
+        </button>
+      </div>
+      <div className="relative h-[400px] w-full  my-10">
+      <Image 
+          src={img} 
+          alt="ME" 
+          
+          className="absolute  h-full object-contain z-20  right-0"
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      </div>
+  </div>
   );
 }
